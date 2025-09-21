14cd330f5587542d04ae1198ea52f2bab9d4dfd8
BrunoCavalcanteNuness <90936373+BrunoCavalcanteNuness@users.noreply.github.com>
2025-09-21 23:29:20 +0000

    TrabalhoAtt_21_09

---
+++ ProjetoStack/DetailsScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default function DatailsScreen({navigation, route}) {
+  const { produtoId } = route.params;
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🏣 Tela de Detalhes</Text>
+      <Text>ID do Produto: {produtoId}</Text>
+      <Button 
+        title="Ir para Perfil" 
+        onPress={() => navigation.navigate('Perfil')} 
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
